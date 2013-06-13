c8ba0a3b873566a7f01b486ee1cfed9003d44611
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-06-13 19:35:29 -0400

    Input, output, research outline

---
+++ mining.r
@@ -0,0 +1,19 @@
+#### Inputs
+# vek, pohlavi
+# uz nasporeno
+# příjem
+# navykove latky
+# povolani (specificke, industry, ...), popr. doba nezamestnanosti
+
+# jaký důchod (v dnešních korunách na mesic) chceš,
+# v kolika letech odejit do duchodu,
+# kolik dětí
+
+#### Research
+# vek, pohlaví -> stredni doba doziti (CSU)
+# životní styl (kuřák, abstinent...) -> očekávaný věk dožití (studie)
+# povolání, industry -> očekávaná budoucí mzda (CSU)
+# stredni hodnota inflace
+
+#### Output
+# kolik šetřit na důchod

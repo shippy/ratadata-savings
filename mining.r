e4da8e077b5a8a5e87b770d2a824e0ef5c9384de
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-06-13 19:42:53 -0400

    Added research items

--- mining.r
+++ mining.r
@@ -2,6 +2,7 @@
 # vek, pohlavi
 # uz nasporeno
 # příjem
+# mesicni vydaje (dluzni splatka)
 # navykove latky
 # povolani (specificke, industry, ...), popr. doba nezamestnanosti
 
@@ -9,11 +10,16 @@
 # v kolika letech odejit do duchodu,
 # kolik dětí
 
+#### Formula
+
+
 #### Research
 # vek, pohlaví -> stredni doba doziti (CSU)
 # životní styl (kuřák, abstinent...) -> očekávaný věk dožití (studie)
 # povolání, industry -> očekávaná budoucí mzda (CSU)
 # stredni hodnota inflace
+# danova zatez na prijem
+# statni pridavek na duchodovy vklad (v zavislosti na platu)
 
 #### Output
 # kolik šetřit na důchod

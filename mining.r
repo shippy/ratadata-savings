56ec1170788091853699a2742436f110eda7de9b
Lenkah <lenkahabetinova@hotmail.com>
2013-06-14 15:01:50 +0300

    Update mining.r

--- mining.r
+++ mining.r
@@ -1,12 +1,17 @@
 #### Inputs
-# vek, pohlavi
-# uz nasporeno
-# příjem
+# vek, pohlavi, dalsi charakteristiky na zaklade kterych pocitame delku zivota (kurak, vzdelani...)
+# uz nasporeno, uroky na soucasnych usporach (sporici ucet, akcie, dluhopisy)
+# příjem+ predpokladany rust prijmu (but ma uzivatel svuj odhad, nebu mu zadame nejaky my na zaklade odvetvi)
+# dalsi zdroje prijmu (pronajem domu, vdovsky duchod,...)
+# duchod od zamestnavatele
 # mesicni vydaje (dluzni splatka)
-# navykove latky
-# povolani (specificke, industry, ...), popr. doba nezamestnanosti
+# povolani (specificke, industry, ...), popr. doba nezamestnanosti -- jako planovana?? nebo jako buffer??
 
-# jaký důchod (v dnešních korunách na mesic) chceš,
+# jaký důchod (v dnešních korunách na mesic) chceš-- tam bych zahrnula ty navykove latky
+# dedicna choroba v rodine - takova, kterou nehradi pojistovna --> buffer (jednorazovy)
+# pokud NE investice do perpetuity - buffer na kolik let v pripade dlouhovekosti (mozna bychom mohli davat extra treba 90% confidence interval)
+# preferovane invstice (ucet, dluhopisy...)
+# majetek planovany k prodeji (umelecka dila,...)
 # v kolika letech odejit do duchodu,
 # kolik dětí
 
@@ -14,12 +19,19 @@
 
 
 #### Research
-# vek, pohlaví -> stredni doba doziti (CSU)
+# vek, pohlaví, popr. dalsi charakteristiky -> stredni doba doziti (CSU)
 # životní styl (kuřák, abstinent...) -> očekávaný věk dožití (studie)
 # povolání, industry -> očekávaná budoucí mzda (CSU)
 # stredni hodnota inflace
 # danova zatez na prijem
 # statni pridavek na duchodovy vklad (v zavislosti na platu)
+# vynosy na ruznych typech investic
+# basic statni duchod (defaultni), jak to funguje s detma
+# caste choroby ve stari - co nehradi pojistovna a co muzou byt velky sok
 
 #### Output
 # kolik šetřit na důchod
+# udelala bzch tech vypoctu vic --
+# 1) duchod, pokud by dotycny mel vyzit jen z toho, co dostane od zamestnavatele, od statu, a co uz ma
+# 2) nejoptimistictejsi scenar: netreba zadny buffer, protoze zemre dle ocekavani a nebude mit velke zdravotni vydaje
+# 3) opatrny scenar: buffer na longevity i zdravotni vydaje

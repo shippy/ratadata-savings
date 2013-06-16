e0c8434cb6ec2bd90523a03890b4656edbe860f1
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-06-16 14:53:36 -0400

    Initial PHP frontend to-do list

---
+++ frontend/index.php
@@ -0,0 +1,9 @@
+<?php
+# Get a PHP mini-MVC
+# Get a simple form plugin
+# Implement a non-jQuery version of things
+# Figure out how to make the version into a jQuery version of things
+# Prettify
+
+
+?>

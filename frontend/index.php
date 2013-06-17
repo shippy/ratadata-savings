5d9fa195ef40489672f6b9498f5a85d2d3d51744
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-06-16 23:30:32 -0400

    Installed Silex framework with Twig via Composer, set up basic (M)VC

--- frontend/index.php
+++ frontend/index.php
@@ -5,5 +5,40 @@
 # Figure out how to make the version into a jQuery version of things
 # Prettify
 
+require_once __DIR__.'/vendor/autoload.php';
 
+$app = new Silex\Application();
+
+$app['debug'] = true;
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+####### Controllers
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+
+$app->get('/input', function() use ($app) {
+	return $app['twig']->render('input.html.twig');
+}
+
+$app->post('/process', function(Request $req) use ($app) {
+	
+})
+
+$app->get('/clear', function() use ($app) {
+	// clean data
+	return $app->redirect('/input');
+})
+
+$app->get('/output', function() use ($app) {
+	return $app['twig']->render('output.html.twig');
+})
+
+$app->run();
 ?>

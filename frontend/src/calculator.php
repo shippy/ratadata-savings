6550b67436e0e0a3208a5ec17c5d89420360b431
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-06-17 17:14:25 -0400

    Minor descriptive changes

--- frontend/src/calculator.php
+++ frontend/src/calculator.php
@@ -1,12 +1,19 @@
 <?php
-namespace Savings/Calculator;
+namespace Savings\Calculator;
 
+/* formula: from \big(\frac{1+s}{1+i}\big)^{12(R-a)}S
+			+ \sum_{j={12a}}^{12R} x\big(\frac{1+s}{1+i}\big)^{j-12a}
+			= \sum_{j={12R}}^{12T} D(1+i)^{j} 
+			to
+			x = \frac{\sum_{j={12R}}^{12T} D(1+i)^\frac{j}{12}
+			- \big(\frac{1+s}{1+i}\big)^{12(R-a)}S}
+			{12(R-a) \sum_{j={12a}}^{12R} \big(\frac{1+s}{1+i}\big)^{\frac{j}{12}-a}} */
 function getMonthlySavings($age_current, $age_retirement, $age_terminal,
 		  $rate_inflation, $rate_interest, // yearly
 		  $monthly_current, $monthly_desired, // income
 		  $savings_current) {
 	
-	$real_rate = (1+$rate_interest) / (1+$rate_inflation);
+	$real_rate = (1+$rate_interest) / (1+$rate_inflation); // yearly
 	
 	$desired_total = 0;
 	for ($i = $age_retirement; $i <= $age_terminal; $i++) {
@@ -22,6 +29,6 @@ function getMonthlySavings($age_current, $age_retirement, $age_terminal,
 	}
 	$discounting *= 12*($age_retirement - $age_current);
 	
-	return ($desired_total - $savings_value_at_retirement) / $discounting;
+	return (($desired_total - $savings_value_at_retirement) / $discounting);
 }
 ?>

119b7016f04bd6b5e2b0840c5039f12755960146
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-06-17 02:15:48 -0400

    Reorganization of files

---
+++ frontend/src/app.php
@@ -0,0 +1,40 @@
+<?php
+# Get a PHP mini-MVC
+# Get a simple form plugin
+# Implement a non-jQuery version of things
+# Figure out how to make the version into a jQuery version of things
+# Prettify
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+/* Controllers */
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+
+$app->get('/input', function() use ($app) {
+	return $app['twig']->render('input.html.twig');
+});
+
+$app->post('/process', function(Request $req) use ($app) {
+	return false;
+});
+
+$app->get('/clear', function() use ($app) {
+	// clean data
+	return $app->redirect('/input');
+});
+
+$app->get('/output', function() use ($app) {
+	return $app['twig']->render('output.html.twig');
+});
+?>
